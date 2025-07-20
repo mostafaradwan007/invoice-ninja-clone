409efba39d3d7830567fceffa1bae728b1128463
mostafa <mostafaradwan@students.du.edu.eg>
2025-07-20 19:02:02 +0300

    Initial working page

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Invoice System</title>
+</head>
+<body>
+    <h1>Welcome to the Invoice System</h1>
+    <p>This is a basic starting point.</p>
+</body>
+</html>
